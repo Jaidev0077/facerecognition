bb26c77945055eda34e42002249bd4c277903341
Jaidev0077 <jaidev0077@gmail.com>
2020-09-21 19:07:04 -0700

    added face recognition api

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,11 @@
 import React from "react";
-
-const FaceRecognition =({imageUrl})=>{
+import './FaceRecognition.css'
+const FaceRecognition =({ box, imageUrl})=>{
     return(
         <div className= 'center ma'>
             <div className = 'absolute mt2'>
-                <img className = "image" src={imageUrl } width = '500px' height = 'auto' alt=" "/>
+                <img id = 'inputImage' className = "image" src={imageUrl } width = '500px' height = 'auto' alt=" "/>
+                <div className = 'bounding_box' style = {{top:box.topRow, right:box.rightCol, bottom:box.bottomRow, left:box.leftCol}}></div>
             </div>
         </div>
     );
